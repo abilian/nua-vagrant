f901d4b3ca52d2214a91aed4d3a77d405f6ed704
Stefane Fermigier <sf@fermigier.com>
2023-04-05 19:43:31 +0200

    cleanup

--- tasks.py
+++ tasks.py
@@ -1,24 +1,24 @@
 import watchfiles
-from invoke import task, Context
+from invoke import Context, task
 
 REMOTE_HOST = "c17.abilian.com"
 REMOTE_DIR = "/home/fermigier/nua-vagrant"
 
-
-@task
-def watch(c: Context, host=None):
-    """Watch for changes a push to a remote server."""
-    host = host or REMOTE_HOST
-
-    excludes = [
-        ".idea", ".vagrant",
-        ".git", ".env", ".venv", ".mypy_cache", ".pytest_cache", "dist", "build",
-        ".nox", ".tox", ".cache", ".coverage",
-    ]
-    excludes_args = " ".join([f"--exclude={e}" for e in excludes])
-    for changes in watchfiles.watch('.'):
-        print("Syncing to remote server...")
-        c.run(f"rsync -e ssh -avz {excludes_args} ./ {host}:{REMOTE_DIR}")
+EXCLUDES = [
+    ".idea",
+    ".vagrant",
+    ".git",
+    ".env",
+    ".venv",
+    ".mypy_cache",
+    ".pytest_cache",
+    "dist",
+    "build",
+    ".nox",
+    ".tox",
+    ".cache",
+    ".coverage",
+]
 
 
 @task
@@ -31,8 +31,16 @@ def test(c: Context):
     print("Cleaning up...")
     c.run("vagrant destroy -f")
 
-    # c.run("vagrant ssh -c 'cd /vagrant/nua-infra-abilian/ && poetry install'")
-    # c.run("vagrant ssh -c 'cd /vagrant/nua-infra-abilian/ && poetry run inv all --host=localhost'")
+
+@task
+def watch(c: Context, host=None):
+    """Watch for changes a push to a remote server."""
+    host = host or REMOTE_HOST
+
+    excludes_args = " ".join([f"--exclude={e}" for e in EXCLUDES])
+    for _ in watchfiles.watch("."):
+        print("Syncing to remote server...")
+        c.run(f"rsync -e ssh -avz {excludes_args} ./ {host}:{REMOTE_DIR}")
 
 
 def ssh(c, cmd):
